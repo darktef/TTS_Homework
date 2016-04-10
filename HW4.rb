4ef22921f5bda031d7fb7236f6c94c82e6f93154
darktef <darktef@gmail.com>
2016-04-09 23:56:18 -0500

    Instance variable, DRY code

--- HW4.rb
+++ HW4.rb
@@ -1,33 +1,36 @@
-product = {
+@product = {
 	"shirt" => 15,
 	"pants" => 12,
 	"belts" => 20,
 	"socks" => 17
 }
 
-def purchase(inven,item,quantity)
-	inven[item] = inven[item] - quantity
-	return inven
+def purchase(item,quantity)
+	@product[item] -= quantity
+	return @product
 end
 
 begin
-	product.each do |i,q|
+	@product.each do |i,q|
 		puts "#{i}: (#{q} in stock)"
 	end
 
 	puts "Choose and item!"
-	order = gets.chomp.downcase
-	if product.has_key?(order) == false
-		puts "Sorry we do not have the item you want. Please enter a valid item." 
+	begin
 		order = gets.chomp.downcase
-	end
+		puts "Sorry we do not have the item you want. Please enter a valid item." if @product.has_key?(order) == false
+	end until @product.has_key?(order) == true
 
 	puts "How many would you like to purchase?"
-	order_num = gets.to_i
+
+	begin
+		order_num = gets.to_i
+		puts "Sorry, we do not have enough #{order} in stock. There are only #{@product[order]} left in the stock." if order_num > @product[order]
+	end until order_num < @product[order]
 
 	puts "You ordered #{order_num} #{order}."
 
-	product = purchase(product,order,order_num)
+	product = purchase(order,order_num)
 
 	puts "Would you like to make more purchase? Enter Y or N:"
 	more = gets.chomp.downcase
@@ -39,4 +42,4 @@ begin
 	end
 end until i == false
 
-puts product
+puts @product
